a2b5de46679dcf7fc6f08a1d9be1c496f6fc9035
Juan Manuel <114026096+JuanManuel1970@users.noreply.github.com>
2023-07-15 16:49:02 -0300

    7

--- index.js
+++ index.js
@@ -12,21 +12,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Soy el Back del MERN</h1>');
+  res.send(`<h1>Soy el Back del MERN</h1>`);
 });
 
 app.post('/usuarios', async (req, res) => {
   console.log(req.body);
   const { nombre, apellido, email, password } = req.body;
 
-  console.log(
-    `Mi nombre es ${nombre}, mi apellido es ${apellido}, mi email es ${email} y el password ${password}`
-  );
+  console.log(`Mi nombre es ${nombre}, mi apellido es ${apellido}, mi email es ${email} y el password ${password}`);
 
-  // Realiza el hash de la contraseña antes de guardarla en la base de datos
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // Crea un nuevo usuario con la contraseña hasheada
   const nuevoUsuario = new Usuario({
     nombre,
     apellido,
@@ -39,25 +35,22 @@ app.post('/usuarios', async (req, res) => {
   await nuevoUsuario.save();
 
   res.json({
-    saludo: 'Dato guardado',
+    saludo: 'Dato guardado'
   });
 });
 
 app.get('/clientes', async (req, res) => {
-  const personas = await Usuario.find(
-    {},
-    {
-      nombre: 1,
-      apellido: 1,
-      email: 1,
-      timestamp: 1,
-    }
-  );
+  const personas = await Usuario.find({}, {
+    "nombre": 1,
+    "apellido": 1,
+    "email": 1,
+    "timestamp": 1
+  });
 
   console.log(personas);
 
   res.json({
-    personas,
+    personas
   });
 });
 
@@ -77,6 +70,56 @@ app.delete('/clientes/:id', async (req, res) => {
     }
   } catch (error) {
     console.log(error);
+  }
+});
+
+app.put('/clientes/:id', async (req, res) => {
+  const id = req.params.id;
+
+  const data = {
+    nombre: req.body.nombre,
+    apellido: req.body.apellido,
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  console.log(data);
+  console.log(id);
+
+  try {
+    const updateUser = await Usuario.findByIdAndUpdate(id, data);
+    console.log(updateUser);
+    if (updateUser) {
+      console.log('Cliente Actualizado');
+      return res.status(200).send();
+    } else {
+      return res.status(404).send();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await Usuario.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+      return res.json({ success: true, user });
+    } else {
+      return res.status(401).json({ error: 'Contraseña incorrecta' });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: 'Error en el servidor' });
   }
 });
 
